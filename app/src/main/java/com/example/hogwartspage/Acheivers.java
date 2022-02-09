dd41458ce984baaa4e3bafdbdcb1922a4284f878
ImSarj <suraj.sharma@rsllearning.com>
2022-02-09 18:45:54 +0530

    Initial

---
+++ app/src/main/java/com/example/hogwartspage/Acheivers.java
@@ -0,0 +1,14 @@
+package com.example.hogwartspage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Acheivers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acheivers);
+    }
+}
---
+++ app/src/main/java/com/example/hogwartspage/Hogwarts_form_relativeLayout.java
@@ -0,0 +1,14 @@
+package com.example.hogwartspage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hogwarts_form_relativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hogwarts_form_relative_layout);
+    }
+}
